0d13c588d6add9743bca33693bcb08afdc920308
dahye828 <reg0828@naver.com>
2021-04-07 11:08:59 +0900

    get sensor list using scrollview + button

--- app/src/main/java/com/example/getsenserlist/MainActivity.java
+++ app/src/main/java/com/example/getsenserlist/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.getsenserlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensors;
+    SensorManager sensorMgr;
+    List<Sensor> sensorList;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.txtSensors);
+        //mTxtSensors.setMovementMethod(new ScrollingMovementMethod());
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+                sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+
+                mTxtSensors.append("(# Sensors:" + sensorList.size() + ")\n\n");
+                for (Sensor sensor : sensorList) { //Enhanced for loop
+                    mTxtSensors.append("Sensor name: " + sensor.getName() + "\n");
+                    mTxtSensors.append("Sensor name: " + sensor.getType() + "\n\n");
+                }
+
+            }
+        });
+
+
     }
 }
